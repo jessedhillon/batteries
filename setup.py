4fb41fc06d47c7178507839a9fdcec2401ca425e
Jesse Dhillon <jesse@deva0.net>
2014-08-06 16:31:17 -0700

    Prepping for PyPI upload

--- setup.py
+++ setup.py
@@ -3,21 +3,22 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-readme = open(os.path.join(here, 'README.rst')).read()
+readme = open(os.path.join(here, 'README.rst'), 'r').read()
+changes = open(os.path.join(here, 'CHANGES.rst'), 'r').read()
 
 requires = ['SQLAlchemy', 'python-dateutil<=1.5']
 
 setup(
-    name='Batteries',
+    name='sqlalchemy-batteries',
     version='0.3',
-    description="Various batteries for Pyramid, SQLAlchemy and Python general programming",
-    long_description=readme,
+    description="Various batteries for SQLAlchemy models",
+    long_description="{}\n\n{}".format(readme, changes),
     classifiers=[
         "Programming Language :: Python",
     ],
     author='Jesse Dhillon',
     author_email='jesse@deva0.net',
-    url='https://github.com/jessedhillon',
+    url='https://github.com/jessedhillon/batteries',
     keywords='util paste',
     packages=find_packages(),
     include_package_data=True,
